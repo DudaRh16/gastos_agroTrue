c39c4e2c3665729ba814591a344141f3e3356a85
DudaRh16 <eduardarhayanefelix2009@gmail.com>
2025-07-25 13:51:36 +0000

    Código atualizado

--- gastos_agro.py
+++ gastos_agro.py
@@ -6,26 +6,32 @@ escolher = input("Escolha uma fruta:")
 if escolher == produtos[0]:
     print(f"Você escolheu o protudo {produtos[0]} e gastará um total de R$40 com sementes ou mudas, R$200 com insumos, R$ 300 com a energia, R$4000 com a mão de obra")
     hectares = int(input(f'Digite a quantidade de hectares que você deseja para a(o) {produtos[0]}:'))
+    custo_total = valor_gasto[0] * hectares
+    print(f"O custo total por hectare é equivalente a: {custo_total}")
 
 elif  escolher == produtos[1]:
     print(f"Você escolheu o protudo {produtos[1]} e gastará um total de R$45 com sementes ou mudas, R$300 com insumos, R$ 750 com a energia, R$2000 com a mão de obra")
     hectares = int(input(f'Digite a quantidade de hectares que você deseja para a(o) {produtos[1]}:'))
-    
+    custo_total = valor_gasto[1] * hectares
+    print(f"O custo total por hectare é equivalente a: {custo_total}")
 
 elif escolher == produtos[2]:
     print(f"Você escolheu o protudo {produtos[2]} e gastará um total de R$35 com sementes ou mudas, R$150 com insumos, R$ 900 com a energia, R$3700 com a mão de obra")
     hectares = int(input(f'Digite a quantidade de hectares que você deseja para a(o) {produtos[2]}:'))
-    
+    custo_total = valor_gasto[2] * hectares
+    print(f"O custo total por hectare é equivalente a: {custo_total}")
 
 elif escolher == produtos[3]:
     print(f"Você escolheu o protudo {produtos[3]} e gastará um total de R$50 com sementes ou mudas, R$500 com insumos, R$ 800 com a energia, R$3800 com a mão de obra")
     hectares = int(input(f'Digite a quantidade de hectares que você deseja para a(o) {produtos[3]}:'))
-    
+    custo_total = valor_gasto[3] * hectares
+    print(f"O custo total por hectare é equivalente a: {custo_total}")
 
 elif escolher == produtos[4]:
     print(f"Você escolheu o produto {produtos[4]} e gastará um total de R$20 com sementes ou mudas, R$160 com insumos, R$ 700 com a energia, R$3400 com a mão de obra")
     hectares = int(input(f'Digite a quantidade de hectares que você deseja para a(o) {produtos[4]}:'))
-    
+    custo_total = valor_gasto[4] * hectares
+    print(f"O custo total por hectare é equivalente a: {custo_total}")
 
 else: 
     print("A fruta não foi encontrada")
